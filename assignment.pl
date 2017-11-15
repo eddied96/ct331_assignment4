4d965406054c334258e654a65b7eaccf702dfb2a
eddied96 <eddied96@users.noreply.github.com>
2017-11-15 17:37:31 +0000

    Full Assignment 4
    bananas

--- assignment.pl
+++ assignment.pl
@@ -1,7 +1,18 @@
-%Definitions for isElementInList(El, List)
+%Defenitions for isElementInList(El, List)
+isElementInList(El, List):-
+    member(El, List).
 
 %Definitions for reverseList(List, ReversedList)
+reverseList([], []).
+reverseList([H|T],Z) :-
+    reverseList(T,Z1),
+    mergeLists(Z1, [H], Z).
 
 %insertElementIntoListEnd(El, List, NewList)
+insertElementIntoListEnd(El, List, NewList):-
+    mergeLists(List, El, NewList).
 
 %Definitions for mergeLists(List1, List2, Merged)
+mergeLists(List1, List2, Merged):-
+   List = [List1, List2],
+    flatten(List, Merged).
